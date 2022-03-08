15d99382a1ae57ac28d10fe040b764e9e9ea9ad8
Chris <99832361+chrisli21@users.noreply.github.com>
2022-03-08 02:53:51 +0000

    finished everytthing but windows pathway and roofs

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(500, 500);
   }
 
   /** 
@@ -16,21 +16,46 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
-  }
+    background(0, 255, 255);
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // Sun
+    fill(246,255,0);
+    float sunX = random(0, height);
+    float sunY = random(0, width);
+    float SunDiameter = random(100,200);
+    ellipse(sunX, sunY, sunDiamter, sunDiameter);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
-}
+    // sun positions
+    boolean sunLeft = sunX >= 250;
+    boolean sunRight = sunX <= 249;
+    boolean sunUp = sunY <= 250;
+    boolean sunDown = sunY >= 249;
+
+    boolean sunTopLeft = sunUp && sunLeft;
+    boolean sunTopRight = sunUp && sunRight;
+    boolean sunBottomLeft = sunDown && sunLeft;
+    boolean sunBottomRight = sunDown && sunRight;
+
+    // background colour if statements
+    if {sunTopLeft} {
+      background(0,255,255);
+    }
+    if (sunTopRight) {
+      background(209,101,0);
+    }
+    if(sunBottomLeft) {
+      background(102, 102, 102);
+    }
+    if(sunBottomRight) {
+      background(71,64,57)
+    }
+
+    // date and time
+    fill(0,0,0);
+    textSize(20);
+    text(day() + "/" + month() + "/" + year() + " " + (hour() + ":" + minute() + ":" + second()), 20, 30);
+
+    // House
+    fill(0,0,0);
+    rect(120,240,220,220);
+    
